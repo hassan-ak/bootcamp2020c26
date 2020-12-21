7eec7de076c1b5d5d653992eeda191209c497830
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-21 23:54:58 +0500

    Complete Class
    1:- Create React app
    2:- Delete un-necessary files
    3:- install dependancy using "npm install --save-dev miragejs","npm i @reduxjs/toolkit","npm i react-redux"
    4:- Completed Class

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      Hello App Component Async
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
